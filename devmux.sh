ea61e95776f75d36e6ef4698116b7d1256e3c3f9
TES <taaviaro@student.uef.fi>
2025-05-24 13:49:35 +0300

    improved the kakoune alias. nesting quotes so fun :V

--- devmux.sh
+++ devmux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-COMMAND="kak -e 'rename-client code' $1"
+COMMAND=$(echo "\"kak -c IDE -e 'rename-client code'\"")
 SESSION_NAME="code"
 if tmux has-session -t $SESSION_NAME 2>/dev/null; then
 	echo "session $SESSION_NAME already exists, attaching to it instead"
@@ -9,17 +9,17 @@ else
 	tmux send-keys -t $SESSION_NAME:1 "kak -clear" C-m
 	tmux send-keys -t $SESSION_NAME:1 "rm -rf /run/user/1000/kakoune-lsp/IDE*" C-m
 	tmux send-keys -t $SESSION_NAME:1 "kak -s IDE -d &" C-m
-	tmux send-keys -t $SESSION_NAME:1 "alias kak='kak -c IDE'" C-m
+	tmux send-keys -t $SESSION_NAME:1 "alias kak=$COMMAND" C-m
 	tmux send-keys -t $SESSION_NAME:1 "clear" C-m
 	if [ -f "$1" ]; then
- 		tmux send-keys -t $SESSION_NAME:1 "$COMMAND" C-m
+ 		tmux send-keys -t $SESSION_NAME:1 "kak $1" C-m
 	fi
 	tmux split-window -h
 	tmux resize-pane -R 35
 	sleep 1
 	tmux send-keys -t $SESSION_NAME:1 "kak -c IDE -e 'rename-client docs'" C-m
 	tmux splitw -v
-	tmux send-keys -t $SESSION_NAME:1 "RPROMPT='' && PROMPT='%F{14} ❱ ' && clear && exa -T" C-m
+	tmux send-keys -t $SESSION_NAME:1 "RPROMPT='' && PROMPT='%F{14} ❱ ' && clear && pfetch" C-m
 	tmux select-pane -L
 	tmux attach-session -t $SESSION_NAME
 fi
